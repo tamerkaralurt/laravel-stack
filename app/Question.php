de1f5fd2f9c8cddf201184c0ba0ddfc38d6e0455
Tamer <tamerkaralurt@gmail.com>
2019-10-28 23:56:38 +0300

    question ve users modeller birbirine ilişkiler olarak bağlandı

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];
+    //$question = Question::find(1);
+    //$question->user->email; gibi kullanılacak.
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
