e97f8042993ad74f39666186325813dbd132eaac
Tamer <tamerkaralurt@gmail.com>
2019-10-29 05:23:40 +0300

    Question modela soru durumları hakkında attribute eklendi ve css değerleri gönderildi

--- app/Question.php
+++ app/Question.php
@@ -30,4 +30,13 @@ class Question extends Model
         // return $this->created_at->forma('d.m.Y');
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0){
+            if($this->best_answer_id) return 'answered-accepted';
+            return "answered";
+        }
+        return 'unanswered';
+    }
 }
