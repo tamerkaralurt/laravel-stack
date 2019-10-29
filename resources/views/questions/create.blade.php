653cdc491c46443f45d88b1ec094781f9c1f8117
Tamer <tamerkaralurt@gmail.com>
2019-10-29 17:05:07 +0300

    question create sayfası oluşturuldu

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>Ask Question</h2>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Go Back</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('questions.store') }}" method="post">
+                            <div class="form-group">
+                                <label for="question-title">Question Title</label>
+                                <input type="text" name="title" id="question-title" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="question-body">Explain you question</label>
+                                <textarea name="body" id="question-body" rows="10" class="form-control"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
