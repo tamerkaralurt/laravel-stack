9e1925a0e0a30949b439e031842e43426fbccbf3
Tamer <tamerkaralurt@gmail.com>
2019-11-03 06:07:04 +0300

    Temiz kod çalışmaları yapıldı, Soru ve cevaplarda en son olan değerin alt çizgisi kaldırıldı

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -6,63 +7,24 @@
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex align-items-center">
-                            <h2>Questions</h2>
+                            <h2>All Questions</h2>
                             <div class="ml-auto">
                                 <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                             </div>
                         </div>
+
                     </div>
                     <div class="card-body">
-                        @include('layouts._messages')
-                        @forelse($questions as $question)
-                            <div class="media">
-                                <div class="d-flex flex-column counters">
-                                    <div class="vote">
-                                        <strong>{{ $question->votes_count }}</strong> {{ Str::plural('vote', $question->votes_count) }}
-                                    </div>
-                                    <div class="status {{ $question->status }}">
-                                        <strong>{{ $question->answers_count }}</strong> {{ Str::plural('answer', $question->answers_count) }}
-                                    </div>
-                                    <div class="view">
-                                        {{ $question->views . " " . Str::plural('view', $question->views) }}
-                                    </div>
-                                </div>
-                                <div class="media-body">
-                                    <div class="d-flex align-items-center">
-                                        <h3 class="mt-0">
-                                            <a href="{{ $question->url }}">{{ $question->title }}</a>
-                                        </h3>
-                                        <div class="ml-auto">
-                                            @can('update', $question)
-                                                <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            @endcan
-                                            @can('delete', $question)
-                                                <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <p class="lead">
-                                        <span>Asked by</span>
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                        <small class="text-muted">{{ $question->created_date }}</small>
-                                    </p>
-                                    <div class="excerpt">{!! $question->excerpt(350) !!}</div>
-                                </div>
-                            </div>
-                            <hr>
+                        @include ('layouts._messages')
+
+                        @forelse ($questions as $question)
+                            @include ('questions._excerpt')
                         @empty
-                            <div class="alert alert-warning m-0">
+                            <div class="alert alert-warning">
                                 <strong>Sorry</strong> There are no questions available.
                             </div>
                         @endforelse
-
-                        <div class="justify-content-center">
-                            {{ $questions->links() }}
-                        </div>
+                        {{ $questions->links() }}
                     </div>
                 </div>
             </div>
