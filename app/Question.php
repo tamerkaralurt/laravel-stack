6e6f17200e9867f7151c4fa3c66e0590e275f71b
Tamer <tamerkaralurt@gmail.com>
2019-11-03 04:08:15 +0300

    Soru ve cevap modelerindeki tekrarlayan fonksiyonları trait ile tek hale getirildi

--- app/Question.php
+++ app/Question.php
@@ -7,6 +7,8 @@ use Str;
 
 class Question extends Model
 {
+    use VotableTrait;
+
     protected $fillable = ['title', 'body'];
     //$question = Question::find(1);
     //$question->user->email; gibi kullanılacak.
@@ -76,20 +78,5 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
