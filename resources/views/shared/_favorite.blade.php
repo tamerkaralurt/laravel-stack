60af792013e0ec7cb9cb80563aebf7c5f7efe349
Tamer <tamerkaralurt@gmail.com>
2019-11-05 00:22:24 +0300

    Favori component haline getirildi

--- resources/views/shared/_favorite.blade.php
+++ resources/views/shared/_favorite.blade.php
@@ -1,7 +1,1 @@
 <favorite :question="{{ $model }}"></favorite>
-<form id="favorite-question-{{ $model->id }}" action="/questions/{{ $model->id }}/favorites" method="POST" style="display:none;">
-    @csrf
-    @if ($model->is_favorited)
-        @method ('DELETE')
-    @endif
-</form>
